7d479257dfd783d665ca97b5aa96093fb1f24404
Chelseaquiin <uzonweze@gmail.com>
2022-11-15 10:32:51 +0000

    Edited the Number in Words

--- NumbersInWords/Program.cs
+++ NumbersInWords/Program.cs
@@ -4,13 +4,12 @@
     {
         static void Main(string[] args)
         {
-            ConvertToWords number1 = new ConvertToWords(299999);
+            ConvertToWords number1 = new ConvertToWords(2999999);
             
             string conversion = number1.Conversion(number1.Number);
             Console.WriteLine(conversion);
 
-            ConvertToNumbers word = new ConvertToNumbers("Two thousand and eight");
-            Console.WriteLine(word.Words);
+            
         }
     }
 }
